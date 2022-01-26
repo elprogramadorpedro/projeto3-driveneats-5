9c5611c378d8cd6a8127360d4e38410561b8a108
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-01-26 13:13:02 -0300

    added select checkmark on first item

--- src/functions.js
+++ src/functions.js
@@ -22,6 +22,8 @@ function scrollItemLeft(number){
 
 function deselectOtherItens(itens){
     for(let item of itens){
+        const selectIcon = item.getElementsByTagName("ion-icon");
+        selectIcon[0].style.display = "none";
         item.style.border = "none";
     }
 }
@@ -30,6 +32,9 @@ function selectItem(number, option){
     const itens = allItens[number].getElementsByClassName("item__opcao");
 
     const item = itens[option];
+    const selectIcon = item.getElementsByTagName("ion-icon");
+    
     deselectOtherItens(itens);
+    selectIcon[0].style.display = "block"
     item.style.border = "5px solid #32B72F";
 }
