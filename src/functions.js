aeeae2d0fe11a048534e321ff8838153dda71be9
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-01-26 13:40:48 -0300

    implemented finish order button visual change

--- src/functions.js
+++ src/functions.js
@@ -1,4 +1,9 @@
+//possui um array referenciando cada linha de itens(prato,bebida,sobremesa)
 const  allItens = document.getElementsByClassName("item");
+
+//usado pra verificar quais itens ja foram selecionados
+const selectedItens = [false,false,false]
+
 
 function scrollItemRight(number){
     
@@ -20,6 +25,7 @@ function scrollItemLeft(number){
     });
 }
 
+//deleseciona todas as outras opções quando um item eh selecionado
 function deselectOtherItens(itens){
     for(let item of itens){
         const selectIcon = item.getElementsByTagName("ion-icon");
@@ -28,7 +34,10 @@ function deselectOtherItens(itens){
     }
 }
 
+//seleciona uma opcao, deseleciona as outras
+//chama o finishOrderHandler pra verificar se o pedido pode ser finalizado
 function selectItem(number, option){
+    finishOrderHandler(number);
     const itens = allItens[number].getElementsByClassName("item__opcao");
 
     const item = itens[option];
@@ -37,4 +46,16 @@ function selectItem(number, option){
     deselectOtherItens(itens);
     selectIcon[0].style.display = "block"
     item.style.border = "5px solid #32B72F";
+}
+
+//se uma opcao em cada item foi selecionado
+//modifica o botao de finalizar pedido
+function finishOrderHandler(number){
+    selectedItens[number] = true;
+    
+    if(selectedItens.every(item => item)){
+        const finishButton = document.getElementsByClassName("finalizar-pedido");
+        finishButton[0].style.background = "#32B72F";
+        finishButton[0].textContent = "Fechar pedido";
+    }
 }
