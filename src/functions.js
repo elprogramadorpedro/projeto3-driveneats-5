1646abdefcb2eadb3e59a194486e61f0383e990c
celso-patiri <a16001@bcc.unifal-mg.edu.br>
2022-01-28 09:03:17 -0300

    implemented select/deselect same item

--- src/functions.js
+++ src/functions.js
@@ -1,125 +1,114 @@
-//possui um array referenciando cada linha de itens(prato,bebida,sobremesa)
-const  allItens = document.getElementsByClassName("item");
-
-//data-identifiers
-const DISH = "dishes"
-const DRINK = "drinks"
-const DESSERT = "desserts"
-
 //usado pra verificar quais itens ja foram selecionados
-const selectedItens = new Array(undefined,undefined,undefined);
+const selectedItens = new Array(undefined, undefined, undefined);
 let finishOrderAvailable = false;
 
-function getIndex(dataIdentifier){ 
-    switch(dataIdentifier){
-        case DISH: return 0;
-        case DRINK: return 1;
-        case DESSERT: return 2;
+//o index referencia a posicao no array selectedItens, indicando qual o tipo do item: prato/drink/sobremesa 
+function getIndex(dataIdentifier) {
+    switch (dataIdentifier) {
+        case "dishes": return 0;
+        case "drinks": return 1;
+        case "desserts": return 2;
     }
 }
 
-//deleseciona a ultima opcao selectionada no prato(dish) atual
-//remove a borda verde e icone do item
-function deselectOtherItem(index){
-    const selectedItem = selectedItens[index];
-    if(selectedItem != undefined){
-        selectedItem.style.boxShadow = "0 0 10px rgba(0, 0, 0, 0.25)";
-        const selectIcon = selectedItem.querySelector("ion-icon");
-        selectIcon.style.display = "none";
+//seleciona o item atual, deseleciona o item previamente selecionado
+function selectItem(itemSelecionado) {
+    const index = getIndex(itemSelecionado.parentElement.getAttribute("data-identifier"));
+
+    if (selectedItens[index] == itemSelecionado) {
+        selectedItens[index] = undefined;
+    } else {
+        if (selectedItens[index] != undefined) {
+            selectedItens[index].classList.toggle("item-selecionado");
+        }
+        selectedItens[index] = itemSelecionado;
     }
-}
 
-//seleciona uma opcao, chama deselectItem() pra deselecionar a ultima opcao desse prato
-//chama o finishOrderHandler pra verificar se o pedido pode ser finalizado
-function selectItem(item){
-    const selectIcon = item.querySelector("ion-icon");
-
-    //adiciona borda verde e revela o icone na opcao selecionada 
-    selectIcon.style.display = "block"
-    item.style.boxShadow = "inset 0 0 0 5px #32B72F";
-
-    const index = getIndex(item.parentElement.getAttribute("data-identifier"));  
-    if(selectedItens[index] != item){
-        deselectOtherItem(index);
-    }     
-    selectedItens[index] = item;
+    //adiciona/remove borda verde e revela o icone na opcao selecionada 
+    itemSelecionado.classList.toggle("item-selecionado");
     finishOrderHandler();
 }
 
-//se uma opcao em cada item foi selecionado modifica o botao de finalizar pedido
-function finishOrderHandler(){
-    if(selectedItens.every(item => item != undefined)){
-        const finishButton = document.getElementById("finalizar-pedido");
-        finishButton.style.background = "#32B72F";
+//se uma opcao em cada item foi selecionado, modifica o botao de finalizar pedido
+//senao, volta o botao pro estado original
+function finishOrderHandler() {
+    const finishButton = document.getElementById("finalizar-pedido");
+    if (selectedItens.every(item => item != undefined)) {
         finishButton.textContent = "Fechar pedido";
         finishButton.classList.add("item-clicavel");
+        finishButton.classList.add("pedido-pronto");
         finishOrderAvailable = true;
+    } else {
+        finishButton.textContent = "Selecione os 3 itens para finalizar o pedido";
+        finishButton.classList.remove("item-clicavel");
+        finishButton.classList.remove("pedido-pronto");
+        finishOrderAvailable = false;
     }
 }
 
-//Calcula o preco total. Atualiza as informacoes sobre o pedido 
-//nos campos de texto das classes informacao-pedido. 
+//Chama os prompts, calcula o preco total. 
+//Atualiza as informacoes sobre o pediado nos campos de texto da tela de confirmacao. 
 //Muda o display da tela de confirmacao pra ela aparecer
-function abrirTelaConfirmacao(){
-    if(finishOrderAvailable){
-
+function abrirTelaConfirmacao() {
+    if (finishOrderAvailable) {
         const nome = prompt("Qual seu nome? 🤔")
         const endereco = prompt("Diga seu endereço 👀")
 
-        const orderInfo = document.getElementsByClassName("informacao-pedido");
-        const itemNames=[];
+        const orderInfo = document.querySelectorAll(".informacao-pedido");
+        const itemNames = [];
+        console.log(orderInfo)
 
         //pega o nome e preco de todos os itens selecionados
         let precoTotal = 0;
-        for(let i = 0; i<selectedItens.length;i++){
+        for (let i = 0; i < selectedItens.length; i++) {
             const selectedOption = selectedItens[i];
-            
+
             const itemName = selectedOption.querySelector("h1");
             const itemPrice = selectedOption.querySelector("h2");
 
-            const itemInfo = orderInfo[i].getElementsByTagName("h3");
+            const itemInfo = orderInfo[i].querySelectorAll("h3");
             itemInfo[0].textContent = itemName.textContent;
             itemInfo[1].textContent = itemPrice.textContent.replace("R$ ", "");
             itemNames.push(itemName.textContent);
-           
-            precoTotal += parseFloat(itemInfo[1].textContent.replace(",","."));
-        }   
+
+            precoTotal += parseFloat(itemInfo[1].textContent.replace(",", "."));
+        }
 
         const precoFinalElement = document.getElementById("preco-final");
-        const precoFinalString = precoTotal.toFixed(2).toString().replace('.',',');
-        precoFinalElement.textContent = "R$ "+precoFinalString;
-        
+        const precoFinalString = precoTotal.toFixed(2).toString().replace('.', ',');
+        precoFinalElement.textContent = "R$ " + precoFinalString;
+
         const telaConfirmacao = document.getElementById("background-confirmacao");
-        telaConfirmacao.style.display="flex";
+        telaConfirmacao.style.display = "flex";
 
         const confirmButton = document.getElementById("botao-confirmar");
-        confirmButton.addEventListener("click", function(){
+        confirmButton.addEventListener("click", function () {
             redirectToWhatsApp(itemNames, precoTotal.toFixed(2), nome, endereco);
-        });   
+        });
     }
 }
 
 //funcao do botao 'cancelar' na tela de confirmacao
-function cancelarPedido(){
+function cancelarPedido() {
     const telaConfirmacao = document.getElementById("background-confirmacao");
-    telaConfirmacao.style.display="none";
+    telaConfirmacao.style.display = "none";
 }
 
-function redirectToWhatsApp(itemNames, precoFinal, nome, endereco){
+function redirectToWhatsApp(itemNames, precoFinal, nome, endereco) {
     let message = "Olá, gostaria de fazer o pedido:"
-    +"\n - Prato: "+itemNames[0]
-    +"\n - Bebida: "+itemNames[1]
-    +"\n - Sobremesa: "+itemNames[2]
-    +"\nTotal: R$ "+precoFinal;
+        + "\n - Prato: " + itemNames[0]
+        + "\n - Bebida: " + itemNames[1]
+        + "\n - Sobremesa: " + itemNames[2]
+        + "\nTotal: R$ " + precoFinal;
 
-    if(nome != "" || endereco != ""){
-        message+="\n";
-        if(nome != "") message += "\nNome: "+nome;
-        if(endereco != "") message += "\nEndereço: "+endereco;
+    if (nome != "" || endereco != "") {
+        message += "\n";
+        if (nome != "") message += "\nNome: " + nome;
+        if (endereco != "") message += "\nEndereço: " + endereco;
     }
 
     const url = "https://wa.me/5521999521936?text="
-    +encodeURIComponent(message)
+        + encodeURIComponent(message)
 
     window.open(url, "_blank")
 }
